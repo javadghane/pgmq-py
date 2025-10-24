ba6bebeed36d072ac3ef92cfcc8ee5f5d6cf6163
Javad Ghane <hi@javadroid.ir>
2025-10-24 18:07:59 +0330

    Add Swagger specification for REST API

--- src/pgmq/__init__.py
+++ src/pgmq/__init__.py
@@ -1,4 +1,18 @@
+from typing import TYPE_CHECKING, Optional
+
 from pgmq.queue import Message, PGMQueue  # type: ignore
 from pgmq.decorators import transaction, async_transaction
 
-__all__ = ["Message", "PGMQueue", "transaction", "async_transaction"]
+if TYPE_CHECKING:  # pragma: no cover
+    from fastapi import FastAPI
+
+
+def create_app(queue: Optional[PGMQueue] = None):
+    """Create a FastAPI application for interacting with PGMQ."""
+
+    from pgmq.api import create_app as _create_app
+
+    return _create_app(queue=queue)
+
+
+__all__ = ["Message", "PGMQueue", "transaction", "async_transaction", "create_app"]
